09bcab5179ed25ea3f800e619641ed3524610b52
Aleksandr <aleksanches123@mail.ru>
2022-12-04 21:22:45 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        byte clientOS = 1;
-        if (clientOS == 0)
-        {System.out.println("Установите версию приложения для IOS по ссылке");}
-        else if (clientOS == 1)
-        {System.out.println("Установите версию приложения для Android по ссылке");}
+        byte clientOS = 0;
+        short clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+            if (clientDeviceYear < 2015) {
+                System.out.println("Советуем установить облегченную версию приложения для IOS по ссылке");
+            }
+        }
+        else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear < 2015) {
+                System.out.println("Советуем установить облегченную версию приложения для Android по ссылке");
+            }
+        }
+
+
 
     }
 }
